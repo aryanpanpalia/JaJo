d90483d6dd66046373c5e6c70108aff0dda5ebe1
Aryan Panpalia <aryanpanpalia@gmail.com>
2024-06-26 20:45:36 -0400

    Add Filler/Outline for History and Invoice Pages

--- app/client/(drawer)/invoice.jsx
+++ app/client/(drawer)/invoice.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import BottomBar from '../../../components/BottomBar'
 import Header from '../../../components/Header'
 
@@ -7,6 +7,15 @@ export default function Invoice() {
 	return (
 		<View style={styles.container}>
 			<Header label={"Invoice"} />
+			
+			<Text style={styles.label}>Current Invoice</Text>
+			<View style={styles.dummy} />
+			
+			<Text style={styles.label}>Past Invoices</Text>
+			<ScrollView contentContainerStyle={styles.invoices}>
+				{ Array.from({length: 10}).map((_, index) => <View key={index} style={styles.dummy} />) }
+			</ScrollView>
+
 			<BottomBar />
 		</View>
 	)
@@ -16,8 +25,25 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: "center",
-		paddingVertical: 60,
+		paddingTop: 60,
+		paddingBottom: 85,
 		gap: 20,
 		backgroundColor: "white"
 	},
+	label: {
+		fontSize: 18,
+		fontWeight: "700",
+		width: 335,
+		textAlign: "left"
+	},
+	invoices: {
+		width: 335,
+		gap: 10
+	},
+	dummy: {
+		width: 335,
+		height: 100,
+		backgroundColor: "#d9d9d9",
+		borderRadius: 5
+	}
 })
