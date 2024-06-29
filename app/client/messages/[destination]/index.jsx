41ecb4ded05551b6219a539ada5b356f580dad17
Aryan Panpalia <aryanpanpalia@gmail.com>
2024-06-29 18:10:04 -0400

    Extract Chat UI to component

--- app/client/messages/[destination]/index.jsx
+++ app/client/messages/[destination]/index.jsx
@@ -1,7 +1,6 @@
-import {FlatList, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View} from 'react-native'
-import React, {useRef, useState} from 'react'
+import React from 'react'
 import {useLocalSearchParams} from "expo-router";
-import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
+import Chat from "../../../../components/Chat";
 
 const data = {
     locations: [
@@ -37,140 +36,15 @@ const data = {
 export default function Page() {
     const {destination} = useLocalSearchParams()
 
-    const [messages, setMessages] = useState(data[destination])
-    const [message, setMessage] = useState("")
-
-    let messagesRef = useRef()
-
-    const elements = []
-    let prevDateString = "Wed Dec 31 1969"
-    messages.forEach(({message, time}) => {
-        const dateString = time.toDateString()
-        if (dateString !== prevDateString) {
-            elements.push({type: "timestamp", value: dateString})
-            prevDateString = dateString
-        }
-
-        elements.push({type: "message", value: message})
-    })
-
-    function scrollToEnd() {
-        messagesRef.scrollToOffset({offset: 999999999, animated: true})
-    }
-
-    function sendMessage() {
-        const newMessage = {message: message, time: new Date()}
-        setMessages([...messages, newMessage])
-        setMessage("")
-
+    function onMessageSend(newMessage) {
         data[destination].push(newMessage)
     }
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-
-            <View style={styles.header}>
-                <MaterialCommunityIcons name="chevron-left" size={30} color="black" style={styles.chevron}/>
-                <Text style={styles.headerText}>All {destination === "locations" ? "Locations" : "Staff"}</Text>
-            </View>
-
-            <FlatList
-                ref={(ref) => messagesRef = ref}
-                contentContainerStyle={styles.messages}
-                data={elements}
-                renderItem={({item}) =>
-                    item.type === "timestamp" ? (
-                        <View style={styles.timestamp}>
-                            <Text style={styles.timestampText}>{item.value}</Text>
-                        </View>
-                    ) : (
-                        <View style={styles.messageBubble}>
-                            <Text style={styles.messageBubbleText}>{item.value}</Text>
-                        </View>
-                    )
-                }
-                onLayout={scrollToEnd}
-                onContentSizeChange={scrollToEnd}
-            />
-
-            <View style={styles.messageRegion}>
-                <TextInput
-                    style={styles.messageField}
-                    placeholder="Type your message..."
-                    placeholderTextColor={"#898989"}
-                    multiline
-                    value={message}
-                    onChangeText={setMessage}
-                />
-                <Pressable style={styles.sendMessageButton} onPress={sendMessage}>
-                    <Ionicons name="send" size={24} color="black"/>
-                </Pressable>
-            </View>
-        </KeyboardAvoidingView>
+        <Chat
+            title={"All " + (destination === "locations" ? "Locations" : "Staff")}
+            messageHistory={data[destination]}
+            onMessageSend={onMessageSend}
+        />
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        paddingTop: 60,
-        gap: 20,
-        backgroundColor: "white",
-    },
-    header: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        width: 335,
-    },
-    chevron: {
-        position: "absolute",
-        left: -10,
-    },
-    headerText: {
-        fontSize: 24,
-        fontWeight: "600"
-    },
-    messages: {
-        width: 335,
-        alignItems: "flex-end",
-    },
-    timestamp: {
-        width: 335,
-        justifyContent: "center",
-        alignItems: "center",
-        marginVertical: 10
-    },
-    timestampText: {
-        color: "#666"
-    },
-    messageBubble: {
-        maxWidth: 267,
-        backgroundColor: "black",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderRadius: 20,
-        marginVertical: 1
-    },
-    messageBubbleText: {
-        color: "white"
-    },
-    messageRegion: {
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        gap: 5,
-        paddingBottom: 40,
-        paddingHorizontal: 20
-    },
-    messageField: {
-        flex: 1,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
-        padding: 10
-    },
-    sendMessageButton: {
-        marginBottom: 5
-    }
-})
---
+++ components/Chat.jsx
@@ -0,0 +1,142 @@
+import {FlatList, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View} from 'react-native'
+import React, {useRef, useState} from 'react'
+import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
+
+export default function Chat({title, messageHistory, onMessageSend}) {
+    const [messages, setMessages] = useState(messageHistory)
+    const [message, setMessage] = useState("")
+
+    let messagesRef = useRef()
+
+    const elements = []
+    let prevDateString = "Wed Dec 31 1969"
+    messages.forEach(({message, time}) => {
+        const dateString = time.toDateString()
+        if (dateString !== prevDateString) {
+            elements.push({type: "timestamp", value: dateString})
+            prevDateString = dateString
+        }
+
+        elements.push({type: "message", value: message})
+    })
+
+    function scrollToEnd() {
+        messagesRef.scrollToOffset({offset: 999999999, animated: true})
+    }
+
+    function sendMessage() {
+        const newMessage = {message: message, time: new Date()}
+        setMessages([...messages, newMessage])
+        setMessage("")
+
+        onMessageSend(newMessage)
+    }
+
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+            <View style={styles.header}>
+                <MaterialCommunityIcons name="chevron-left" size={30} color="black" style={styles.chevron}/>
+                <Text style={styles.headerText}>{title}</Text>
+            </View>
+
+            <FlatList
+                ref={(ref) => messagesRef = ref}
+                contentContainerStyle={styles.messages}
+                data={elements}
+                renderItem={({item}) =>
+                    item.type === "timestamp" ? (
+                        <View style={styles.timestamp}>
+                            <Text style={styles.timestampText}>{item.value}</Text>
+                        </View>
+                    ) : (
+                        <View style={styles.messageBubble}>
+                            <Text style={styles.messageBubbleText}>{item.value}</Text>
+                        </View>
+                    )
+                }
+                onLayout={scrollToEnd}
+                onContentSizeChange={scrollToEnd}
+            />
+
+            <View style={styles.messageRegion}>
+                <TextInput
+                    style={styles.messageField}
+                    placeholder="Type your message..."
+                    placeholderTextColor={"#898989"}
+                    multiline
+                    value={message}
+                    onChangeText={setMessage}
+                />
+                <Pressable style={styles.sendMessageButton} onPress={sendMessage}>
+                    <Ionicons name="send" size={24} color="black"/>
+                </Pressable>
+            </View>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        paddingTop: 60,
+        gap: 20,
+        backgroundColor: "white",
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 335,
+    },
+    chevron: {
+        position: "absolute",
+        left: -10,
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: "600"
+    },
+    messages: {
+        width: 335,
+        alignItems: "flex-end",
+    },
+    timestamp: {
+        width: 335,
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 10
+    },
+    timestampText: {
+        color: "#666"
+    },
+    messageBubble: {
+        maxWidth: 267,
+        backgroundColor: "black",
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 20,
+        marginVertical: 1
+    },
+    messageBubbleText: {
+        color: "white"
+    },
+    messageRegion: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        gap: 5,
+        paddingBottom: 40,
+        paddingHorizontal: 20
+    },
+    messageField: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10
+    },
+    sendMessageButton: {
+        marginBottom: 5
+    }
+})
