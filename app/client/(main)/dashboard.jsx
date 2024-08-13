d639cfd40ac9387ed820a0a53c6cb6cc84c8ee81
Aryan Panpalia <aryanpanpalia@gmail.com>
2024-08-12 23:32:22 -0400

    Re-did Client dashboard in the style of the Rider dashboard
    TODO: Need to extract commonly used things into components since now both dashboards are nearly identical

--- app/client/(main)/dashboard.jsx
+++ app/client/(main)/dashboard.jsx
@@ -1,174 +1,156 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import {ScrollView, StyleSheet, Text, View} from 'react-native'
+import React, {useState} from 'react'
 import NavigationButton from '../../../components/NavigationButton'
 import BottomBar from '../../../components/client/BottomBar'
-import { useNavigation } from 'expo-router'
 import Header from '../../../components/Header'
 
 const data = {
-	overall: [
-		{label: "Total Revenue", value: "1296.02 MYR"},
-		{label: "Total IFR", value: "82.245%"}
-	],
-	locations: [
-		{name: "Suasana Sentral", revenue: 482.85, IFR: 93.330},
-		{name: "The Sentral Residenses", revenue: 219.41, IFR: 62.589},
-		{name: "The Edge at Polaris", revenue: 398.57, IFR: 88.476},
-		{name: "Olentangy Falls", revenue: 195.19, IFR: 96.882},
-	],
-	riders: [
-		{name: "Teja Singh", revenue: 589.74, IFR: 95.219},
-		{name: "Ramaswamy Pillai", revenue: 706.28, IFR: 86.375},
-	],
-	products: [
-		{name: "Apples", revenue: 482.85, IFR: 93.330},
-		{name: "Bananas", revenue: 219.41, IFR: 62.589},
-		{name: "Milk", revenue: 316.11, IFR: 88.476},
-		{name: "Whole Wheat Flour", revenue: 82.46, IFR: 100},
-		{name: "Paneer", revenue: 195.19, IFR: 96.882},
-		{name: "Honey", revenue: 0, IFR: 0},
-		{name: "Apricots", revenue: 0, IFR: 0},
-		{name: "Papaya", revenue: 0, IFR: 0},
-	]
+    overall: {name: "Panpalia Products", revenue: 1296.02, IFR: 82.245},
+    locations: [
+        {name: "Suasana Sentral", revenue: 482.85, IFR: 93.330},
+        {name: "The Sentral Residenses", revenue: 219.41, IFR: 62.589},
+        {name: "The Edge at Polaris", revenue: 398.57, IFR: 88.476},
+        {name: "Olentangy Falls", revenue: 195.19, IFR: 96.882},
+    ],
+    riders: [
+        {name: "Teja Singh", revenue: 589.74, IFR: 95.219},
+        {name: "Ramaswamy Pillai", revenue: 706.28, IFR: 86.375},
+    ],
+    products: [
+        {name: "Apples", revenue: 482.85, IFR: 93.330},
+        {name: "Bananas", revenue: 219.41, IFR: 62.589},
+        {name: "Milk", revenue: 316.11, IFR: 88.476},
+        {name: "Whole Wheat Flour", revenue: 82.46, IFR: 100},
+        {name: "Paneer", revenue: 195.19, IFR: 96.882},
+        {name: "Honey", revenue: 0, IFR: 0},
+        {name: "Apricots", revenue: 0, IFR: 0},
+        {name: "Papaya", revenue: 0, IFR: 0},
+    ]
 }
 
 const dropdownData = [
-	{label: 'Today', value: 'Today'},
-	{label: 'Week', value: 'Week'},
-	{label: 'Month', value: 'Month'},
+    {label: 'Today', value: 'Today'},
+    {label: 'Week', value: 'Week'},
+    {label: 'Month', value: 'Month'},
 ]
 
 export default function Dashboard() {
     const [timespan, setTimespan] = useState("Today");
-	const [selectedStatistic, setSelectedStatistic] = useState("locations");
+    const [selectedStatistic, setSelectedStatistic] = useState("locations");
 
-	function OverallStatistic(item, index) {
+    function StatisticInformation({item}) {
+        const styles = StyleSheet.create({
+            statisticInformation: {
+                width: "100%",
+                padding: 15,
+                borderRadius: 10,
+                borderWidth: 1,
+                gap: 5
+            },
+            row: {
+                width: "100%",
+                flexDirection: "row",
+                gap: 10
+            },
+            topText: {
+                fontSize: 16,
+                fontWeight: "600"
+            },
+            fill: {
+                flex: 1
+            }
+        })
 
-		const styles = StyleSheet.create({
-			overallStatistic: {
-				width: 150,
-				height: 100,
-				borderWidth: 1,
-				borderRadius: 10,
-			},
-			overallStatisticLabel: {
-				width: "100%",
-				textAlign: "center",
-				fontWeight: "600",
-				position: "absolute",
-				top: 15
-			}, 
-			overallStatisticValue: {
-				width: "100%",
-				textAlign: "center",
-				fontSize: 18,
-				fontWeight: "800",
-				position: "absolute",
-				top: 45
-			},
-		})
-
-		return (
-			<View style={styles.overallStatistic} key={index}>
-				<Text style={styles.overallStatisticLabel}>{item.label}</Text>
-				<Text style={styles.overallStatisticValue}>{item.value}</Text>
-			</View>
-		)
-	}
-
-	function StatisticInformation({item}) {
-
-		const styles = StyleSheet.create({
-			statisticInformation: {
-				width: "100%",
-				height: 80,
-				borderWidth: 1,
-				borderRadius: 10,
-				marginBottom: 10
-			},
-			name: {
-				position: "absolute",
-				left: 25,
-				top: 30,
-				fontSize: 15
-			},
-			revenue: {
-				position: "absolute",
-				right: 25,
-				top: 30,
-				fontSize: 15,
-				fontWeight: "600"
-			},
-			ifr: {
-				position: "absolute",
-				right: 25,
-				top: 50,
-				fontSize: 10,
-			}
-		})
-
-		return (
-			<View style={styles.statisticInformation}> 
-				<Text style={styles.name}>{item.name}</Text>
-				<Text style={styles.revenue}>{item.revenue} MYR</Text>
-				<Text style={styles.ifr}>{item.IFR}% IFR</Text>
-			</View>
-		)
-	}
+        return (
+            <View style={styles.statisticInformation}>
+                <View style={styles.row}>
+                    <Text style={[styles.topText, styles.fill]}>{item.name}</Text>
+                    <Text style={styles.topText}>{item.revenue} MYR</Text>
+                </View>
+                <View style={styles.row}>
+                    <View style={styles.fill}/>
+                    <Text>{item.IFR}% IFR</Text>
+                </View>
+            </View>
+        )
+    }
 
     return (
-		<View style={styles.container}>
-			<Header label={"Dashboard"} dropdownData={dropdownData} dropdownValue={timespan} onDropdownChange={item => setTimespan(item.value)} />
+        <View style={styles.container}>
+            <Header
+                label={"Dashboard"}
+                dropdownData={dropdownData}
+                dropdownValue={timespan}
+                onDropdownChange={item => setTimespan(item.value)}
+                style={styles.header}
+            />
 
-			<View style={styles.overallStatistics}>
-				{ data.overall.map(OverallStatistic) }
-			</View>
+            <View style={styles.statisticSelector}>
+                <NavigationButton
+                    text="By Location"
+                    type="left"
+                    selected={selectedStatistic === "locations"}
+                    onPress={() => setSelectedStatistic("locations")}
+                    style={styles.navigationButton}
+                />
+                <NavigationButton
+                    text="By Rider"
+                    type="center"
+                    selected={selectedStatistic === "riders"}
+                    onPress={() => setSelectedStatistic("riders")}
+                    style={styles.navigationButton}
+                />
+                <NavigationButton
+                    text="By Product"
+                    type="right"
+                    selected={selectedStatistic === "products"}
+                    onPress={() => setSelectedStatistic("products")}
+                    style={styles.navigationButton}
+                />
+            </View>
 
-			<View style={styles.statisticSelector}>
-				<NavigationButton text="By Location" type="left" width={110} height={30} selected={selectedStatistic === "locations"} onPress={() => setSelectedStatistic("locations")} />
-				<NavigationButton text="By Rider" type="center" width={110} height={30} selected={selectedStatistic === "riders"} onPress={() => setSelectedStatistic("riders")} />
-				<NavigationButton text="By Product" type="right" width={110} height={30} selected={selectedStatistic === "products"} onPress={() => setSelectedStatistic("products")} />
-			</View>
+            <ScrollView contentContainerStyle={styles.statistics}>
+                <StatisticInformation item={data.overall}/>
+                <View style={styles.horizontalLine}/>
+                {
+                    data[selectedStatistic].map((item, index) => <StatisticInformation key={index} item={item}/>)
+                }
+            </ScrollView>
 
-			<FlatList 
-				style={styles.statistics}
-				data={data[selectedStatistic]}
-				renderItem={StatisticInformation}
-				keyExtractor={item => item.name}
-				snapToAlignment="start"
-				snapToInterval={90} 
-				maxHeight={450}
-			/>
-
-			<BottomBar selected={"dashboard"} />
-
-		</View>
+            <BottomBar selected={"dashboard"}/>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		paddingVertical: 60,
-		gap: 20,
-		backgroundColor: "white"
-	},
-	overallStatistics: {
-		width: 335,
-		flexDirection: "row",
-		gap: 20,
-		paddingTop: 15,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	statisticSelector: {
-		width: 335,
-		flexDirection: "row",
-		justifyContent: "space-between",
-	},
-	statistics: {
-		width: 335,
-		maxHeight: 450
-	}
+    container: {
+        flex: 1,
+        gap: 20,
+        paddingTop: 60,
+        paddingBottom: 80,
+        paddingHorizontal: 25,
+        backgroundColor: "white"
+    },
+    header: {
+        width: "100%"
+    },
+    statisticSelector: {
+        flexDirection: "row",
+        width: "100%",
+        gap: 5
+    },
+    navigationButton: {
+        flex: 1,
+        height: 30
+    },
+    horizontalLine: {
+        width: "100%",
+        height: 1,
+        backgroundColor: "black"
+    },
+    statistics: {
+        width: "100%",
+        gap: 10,
+        paddingBottom: 10
+    }
 });
--- components/client/BottomBar.jsx
+++ components/client/BottomBar.jsx
@@ -1,6 +1,6 @@
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import {Image, Pressable, StyleSheet, View} from 'react-native'
 import React from 'react'
-import { router } from 'expo-router';
+import {router} from 'expo-router';
 
 const dashboard = require('../../assets/images/nav/DashboardLayout.png');
 const dashboardFilled = require('../../assets/images/nav/DashboardLayoutFilled.png');
@@ -18,47 +18,51 @@ const account = require('../../assets/images/nav/Account.png');
 const accountFilled = require('../../assets/images/nav/AccountFilled.png');
 
 export default function BottomBar({selected}) {
+    const dashboardImage = selected === "dashboard" ? dashboardFilled : dashboard
+    const orderImage = selected === "order" ? orderFilled : order
+    const riderImage = selected === "rider" ? riderFilled : rider
+    const messagingImage = selected === "messaging" ? messagingFilled : messaging
+    const accountImage = selected === "account" ? accountFilled : account
 
-	const dashboardImage = selected === "dashboard" ? dashboardFilled : dashboard
-	const orderImage = selected === "order" ? orderFilled : order
-	const riderImage = selected === "rider" ? riderFilled : rider
-	const messagingImage = selected === "messaging" ? messagingFilled : messaging
-	const accountImage = selected === "account" ? accountFilled : account
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={() => router.navigate("/client/dashboard")}>
+                <Image source={dashboardImage} style={styles.image}/>
+            </Pressable>
 
-  	return (
-		<View style={styles.container}>
-			<Pressable onPress={() => router.navigate("/client/dashboard")}>
-				<Image source={dashboardImage} style={{width: 30, height: 30}} />
-			</Pressable>
-			
-			<Pressable onPress={() => router.navigate("/client/order-administration")}>
-				<Image source={orderImage} style={{width: 30, height: 30}} />
-			</Pressable>
-			
-			<Pressable onPress={() => router.navigate("/client/assign-riders")}>
-				<Image source={riderImage} style={{width: 30, height: 30}} />
-			</Pressable>
-			
-			<Pressable onPress={() => router.navigate("/client/messaging")}>
-				<Image source={messagingImage} style={{width: 30, height: 30}} />
-			</Pressable>
-			
-			<Pressable onPress={() => router.navigate("/client/account")}>
-				<Image source={accountImage} style={{width: 30, height: 30}} />
-			</Pressable>
-		</View>
-	)	
+            <Pressable onPress={() => router.navigate("/client/order-administration")}>
+                <Image source={orderImage} style={styles.image}/>
+            </Pressable>
+
+            <Pressable onPress={() => router.navigate("/client/assign-riders")}>
+                <Image source={riderImage} style={styles.image}/>
+            </Pressable>
+
+            <Pressable onPress={() => router.navigate("/client/messaging")}>
+                <Image source={messagingImage} style={styles.image}/>
+            </Pressable>
+
+            <Pressable onPress={() => router.navigate("/client/account")}>
+                <Image source={accountImage} style={styles.image}/>
+            </Pressable>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		width: "100%",
-		height: 80,
-		flexDirection: "row",
-		justifyContent: "space-evenly",
-		position: "absolute",
-		bottom: 0,
-		borderTopWidth: 2,
-		padding: 10
-	}
+    container: {
+        height: 80,
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        borderTopWidth: 2,
+        padding: 10,
+    },
+    image: {
+        width: 30,
+        height: 30
+    }
 })
